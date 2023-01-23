7fceb895d9a4e7a41d0f3bff2a721d0217c67ef6
Nicolas Perrin <Nicolas.perrin83110@gmail.com>
2023-01-23 23:16:53 +0100

    Ajout d'une requête SPARQL fédérée vers DBPedia

--- DataLifting/rdf_lift.py
+++ DataLifting/rdf_lift.py
@@ -1,8 +1,10 @@
 import json
+import requests
 from tqdm import tqdm
 from rdflib import Graph, Namespace, URIRef, Literal
 from rdflib.namespace import RDF, RDFS, XSD
 from pyshacl import validate
+import imdb
 
 
 def to_camel_case(string):
@@ -38,6 +40,9 @@ def sparql_request_from_topics(topics):
 
 g = Graph()
 
+# On crée une instance d'IMDb
+ia = imdb.IMDb()
+
 # On charge les schémas
 g.parse("../ontology/schema/MergedSchemas.ttl", format="turtle")
 g.parse("../DataEnriching/topics.ttl", format="turtle")
@@ -67,6 +72,7 @@ else:
 with open("../DataEnriching/movies_enriched.json") as json_file:
     data = json.load(json_file)
     for item in tqdm(data):
+
         film = URIRef("http://example.com/movie/" + str(item["id"]))
         g.add((film, RDF.type, movie.Movie))
         g.add((film, movie.Title, Literal(item["title"])))
@@ -79,6 +85,36 @@ with open("../DataEnriching/movies_enriched.json") as json_file:
         g.add((film, movie.VoteCount, Literal(item["vote_count"])))
         g.add((film, movie.OriginalLanguage, Literal(item["original_language"])))
         g.add((film, movie.OriginalTitle, Literal(item["original_title"])))
+
+        '''
+        On fait une requête SPARQL fédérée sur DBPedia pour enrichir le graphe
+        avec la liste d'acteurs pour un film donné
+        '''
+        sparql_query = """
+        PREFIX dbo: <http://dbpedia.org/ontology/>
+        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+
+        SELECT ?film ?actor WHERE {
+            SERVICE silent <http://dbpedia.org/sparql> {
+                ?film a dbo:Film ;
+                    foaf:name "Film name"@en ;
+                    dbo:starring ?actor .
+            }
+        }
+        """
+
+        film_title = item["title"].replace('"', '')
+        sparql_query = sparql_query.replace("Film name", film_title)
+        #print(sparql_query)
+
+        qres = g.query(sparql_query)
+        for row in qres:
+            film = URIRef("http://example.com/movie/" + str(item["id"]))
+            actor_uri = row[1]
+            print("URI de l'Acteur" + str(actor_uri))
+            g.add((film, movie.Actor, actor_uri))
+            g.add((actor_uri, RDF.type, movie.Actor))
+
 
         if 'topics' in item:
             # Ajouter des triples pour les sujets
